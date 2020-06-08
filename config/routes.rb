0400373c8cacb7a3372c85d29e8e2be85faa127c
mitsukildsen <water-2484mitsukildsen@i.softbank.jp>
2020-06-08 18:05:38 +0900

    favorite_comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about' => 'home#about'
   resources :users,only: [:show,:index,:edit,:update,:create]
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+end
 end
