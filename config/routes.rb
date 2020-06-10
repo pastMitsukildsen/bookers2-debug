0a07747ce845d0dd09c01790dcb783b2427856dc
mitsukildsen <water-2484mitsukildsen@i.softbank.jp>
2020-06-10 21:53:58 +0900

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about' => 'home#about'
+  get 'users/:id/follows' => 'users#follows', as: "users_follows"
+  get 'users/:id/followers' => 'users#followers', as: "users_followers"
+  resources :relationships, only: [:create, :destroy]
   resources :users,only: [:show,:index,:edit,:update,:create]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
-end
+  end
 end
